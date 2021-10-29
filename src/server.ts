9500a5a1e2e9700c5a491312020ac514a75a76a7
Julia Loeschel <juloeschel@gmail.com>
2021-10-29 13:13:00 +0200

    Add and Delete a course

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,52 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
+import { connectDatabase, getUserCollection } from './utils/database';
+
+if (!process.env.KEY_URL_MONGOBD)
+  throw new Error('no KEY_URL_MONGOBD provided');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (_req, res) => {
-  res.send('Hello World!');
+app.use(express.json());
+
+app.post('/Kurse', async (request, response) => {
+  const newCourse = request.body;
+  const doesCourseExist = await getUserCollection().findOne({
+    classNumber: newCourse.classNumber,
+  });
+  if (doesCourseExist) {
+    response.send(
+      `Course with the number ${newCourse.classNumber} already exists`
+    );
+  } else {
+    await getUserCollection().insertOne(newCourse);
+    response.send(`Hello! new Course with name ${newCourse.cursname} added!`);
+  }
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.delete('/Kurse/:classNumber', async (request, response) => {
+  const classNumberToBeDeleted = request.params.classNumber;
+  const doesCourseExist = await getUserCollection().findOne({
+    classNumber: classNumberToBeDeleted,
+  });
+  if (doesCourseExist) {
+    await getUserCollection().deleteOne({
+      classNumber: classNumberToBeDeleted,
+    });
+    response.send(
+      `DELETE SUCCESSFULL! Course with Number ${classNumberToBeDeleted} deleted!`
+    );
+  } else
+    response.send(
+      `DELETE FAIL! Course with Number ${classNumberToBeDeleted} does not exist!`
+    );
 });
+
+connectDatabase(process.env.KEY_URL_MONGOBD).then(() =>
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  })
+);
